875034fc3f7e173fd6aefe5b3e383eccb4d7baf2
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2025-02-02 01:57:24 +0100

    Move PerformanceMonitor to its own file

--- Sources/SwiftPy/Interpreter.swift
+++ Sources/SwiftPy/Interpreter.swift
@@ -7,34 +7,6 @@
 
 import pocketpy
 import Foundation
-import OSLog
-
-@available(macOS 12.0, iOS 15.0, *)
-@MainActor
-final class PerformanceMonitor {
-    static let standard = PerformanceMonitor()
-    
-    let signposter: OSSignposter
-    var state: OSSignpostIntervalState?
-    var startTime: DispatchTime = .now()
-    static var executionTime: UInt64 = 0
-    
-    init() {
-        signposter = OSSignposter(logger: Logger(OSLog(subsystem: "com.felfoldy.SwiftPy", category: .pointsOfInterest)))
-    }
-    
-    @inlinable static func begin() {
-        standard.state = standard.signposter.beginInterval("Python")
-        standard.startTime = DispatchTime.now()
-    }
-    
-    @inlinable static func end() {
-        if let state = standard.state {
-            executionTime = DispatchTime.now().uptimeNanoseconds - standard.startTime.uptimeNanoseconds
-            standard.signposter.endInterval("Python", state)
-        }
-    }
-}
 
 @MainActor
 public final class Interpreter {
