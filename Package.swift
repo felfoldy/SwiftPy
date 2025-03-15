886ff420d561fa04d725a1672aeb0383be8f31d5
Tibor Felföldy <tiboofficial@gmail.com>
2025-03-15 21:13:26 +0100

    Update LogTools

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.0"),
-        .package(url: "https://github.com/felfoldy/LogTools.git", from: "1.0.2")
+        .package(url: "https://github.com/felfoldy/LogTools.git", from: "1.1.0")
     ],
     targets: [
         .target(
